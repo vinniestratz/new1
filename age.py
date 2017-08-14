666ea4df2002003093f35431c471a4d74b52354d
vinniestratz <vinniestratz@gmail.com>
2017-08-13 23:12:55 -0400

    need assitance on this file

---
+++ age.py
@@ -0,0 +1,6 @@
+print 'You must be at least 31 years old to enter this site.'
+age = raw_input('What is your age?')
+if age >= 31:
+	print 'Welcome!'
+else:
+	print 'Come back when you are of proper age.'
